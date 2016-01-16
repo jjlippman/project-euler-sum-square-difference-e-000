9eca02e3f71365994bdb11a0e9e4d90ee98bbf99
jjlippman <jjlippman@earthlink.net>
2016-01-16 16:16:53 -0700

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+
+  def initialize(limit)
+    @limit=limit
+  end
+
+def difference
+     i=1
+    
+    total_squared = 0
+    squared_total = 0
+
+    while i <= @limit do
+      total_squared += i * i
+      i+=1
+    end
+    
+    i=1
+    while i <= @limit do 
+      squared_total += i
+      i+=1
+    end
+    squared_total *= squared_total
+
+    
+    squared_total - total_squared
+  end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,23 @@
 # Implement your procedural solution here!
+def sum_square_difference(limit)
+  
+  total_squared = 0
+  squared_total = 0
+  
+  i=1
+  while i <= limit do
+    total_squared += i * i
+    i+=1
+  end
+  
+  i=1
+  while i <= limit do 
+    squared_total += i
+    i+=1
+  end
+  squared_total *= squared_total
+
+  
+  squared_total - total_squared
+end
+
